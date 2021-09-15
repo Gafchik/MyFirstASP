1004d2914e662584b282f86fbcaa0fccf29ee6f5
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-09-15 19:14:46 +0300

    в бд пишет только одну букву

--- MyFirstASP/Controllers/HomeController.cs
+++ MyFirstASP/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyFirstASP.Models;
+using MyFirstASP.Models.Users;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,6 +12,7 @@ namespace MyFirstASP.Controllers
 {
     public class HomeController : Controller
     {
+        private UserRepo userRepo = new UserRepo();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -18,8 +20,24 @@ namespace MyFirstASP.Controllers
             _logger = logger;
         }
 
+
         public IActionResult Index()
         {
+            Task.Run(() =>
+            {
+                 long ip = Request.HttpContext.Connection.RemoteIpAddress.Address;
+              //  long ip = 1;
+                 string date = DateTime.Now.Day.ToString() + ":" + DateTime.Now.Month.ToString() + ":" + DateTime.Now.Year.ToString();
+                  string time = DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString();
+               // string date = "test";
+               // string time = "test2";
+                userRepo.Autorez(new User()
+                {
+                    IP = ip,
+                    Date = date,
+                    Time = time
+                });
+            });
             return View();
         }
 
---
+++ MyFirstASP/Models/Users/UserRepo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASP.Models.Users
+{
+    public class UserRepo
+    {
+        string conSTR = "Data Source=SQL5080.site4now.net;Initial Catalog=db_a7920a_mkonjibhu;User Id=db_a7920a_mkonjibhu_admin;Password=QwertyuioP123";
+        public void Autorez(User new_user)
+        {        
+            using (IDbConnection db = new SqlConnection(conSTR))
+            {
+                db.Open();
+                using (var transaction = db.BeginTransaction())
+                {
+                    try
+                    {
+                        var sqlQuery = "exec [AddAuto] @Date , @Time, @IP ";
+                        var values = new
+                        {
+                            Date = new_user.Date,
+                            Time = new_user.Time,
+                            IP = new_user.IP
+                        };
+                        db.Query(sqlQuery, values, transaction);
+                        transaction.Commit();                 
+                    }
+                    catch (Exception) {  transaction.Rollback();  }
+                }
+            }
+          
+        }
+    }
+}
